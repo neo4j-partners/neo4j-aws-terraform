2b2ce4164877c889d4f243a5370a0d67c98a87fd
Andrei Stefanie <andrei.stefanie@cyscale.com>
2023-10-19 13:37:03 +0300

    fix: add neo4j password lifecycle and exclude punctuation

--- secrets.tf
+++ secrets.tf
@@ -1,12 +1,17 @@
 data "aws_secretsmanager_random_password" "this" {
-  password_length = 16
+  password_length     = 16
+  exclude_punctuation = true
 }
 
 resource "aws_secretsmanager_secret" "this" {
-  name = "neo4j-creds"
+  name = "neo4j-password"
 }
 
 resource "aws_secretsmanager_secret_version" "this" {
   secret_id     = aws_secretsmanager_secret.this.id
   secret_string = data.aws_secretsmanager_random_password.this.random_password
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
 }
